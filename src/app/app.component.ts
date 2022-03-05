6287be68e96a300882c40de023faef2d7f307411
sanchesaline6 <sanches.aline6@gmail.com>
2022-03-05 18:52:08 -0300

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  //Variável da página
+  pagina1: boolean = true;
+
+  //Função para alterar template
+  alterarPagina() {
+    this.pagina1 = !this.pagina1;
+  }
 }
